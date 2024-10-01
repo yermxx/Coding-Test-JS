828155684ac72e073f30c138c03369fd87e0f729
yermxx <66yrlee99@gmail.com>
2024-10-01 21:14:25 +0900

    Stack 문제 풀이 - 1

--- Stack/1. 괄호 짝 맞추기.js
+++ Stack/1. 괄호 짝 맞추기.js
@@ -1,23 +1,20 @@
 const assert = require('assert');
 
 const solution = (s) => {
-  if (s.length % 2 !== 0) return false;
-  if (s[s.length - 1] === '(') return false;
-
   const stack = [];
 
   for (let v of s) {
-    if (v === '(') {
-      stack.push(v);
-    } else {
-      if (stack.length === 0) return false;
-      else stack.pop();
+    if (v === '(') stack.push(v);
+    else if (v === ')') {
+      if (stack.length === 0) {
+        return false;
+      } else {
+        stack.pop();
+      }
     }
   }
   return !stack.length;
 };
 
-assert.deepEqual(solution('()()'), true);
-assert.deepEqual(solution('(())()'), true);
-assert.deepEqual(solution(')()('), false);
-assert.deepEqual(solution('(()('), false);
+assert.strictEqual(solution('(())()'), true);
+assert.strictEqual(solution('((())()'), false);
